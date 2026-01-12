e263f76e7c511361ffd1aad0baff10bcf7658083
vs-machado <vinicius.s.machado@protonmail.com>
2026-01-12 13:11:09 -0300

    test blogs list extracted to a helper file

--- part4/blog-list/blog-list-backend/tests/blogs_test.js
+++ part4/blog-list/blog-list-backend/tests/blogs_test.js
@@ -10,58 +10,15 @@ test('dummy returns one', () => {
 })
 
 describe('totalLikes', () => {
-  const listWithMultipleBlogs = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: '5a422bb71b54a676234d17f9',
-      title: 'Reflections on Trusting Trust',
-      author: 'Ken Thompson',
-      url: 'https://www.archive.ece.cmu.edu/~ganger/712.fall02/papers/p761-thompson.pdf',
-      likes: 12,
-      __v: 0
-    },
-    {
-      _id: '5a422cc71b54a676234d17fa',
-      title: 'The Mythical Man-Month',
-      author: 'Frederick P. Brooks Jr.',
-      url: 'https://archive.org/details/mythicalmanmonth00broo',
-      likes: 20,
-      __v: 0
-    },
-    {
-      _id: '5a422dd71b54a676234d17fb',
-      title: 'A Note on Distributed Computing',
-      author: 'Leslie Lamport',
-      url: 'https://lamport.azurewebsites.net/pubs/distributed-system.txt',
-      likes: 7,
-      __v: 0
-    },
-    {
-      _id: '5a422ee71b54a676234d17fc',
-      title: 'No Silver Bullet — Essence and Accidents of Software Engineering',
-      author: 'Frederick P. Brooks Jr.',
-      url: 'https://www.cs.unc.edu/techreports/86-020.pdf',
-      likes: 15,
-      __v: 0
-    }
-  ]
-
   const emptyList = []
 
   test('when list has only one post, equals the likes of that', () => {
-    const result = listHelper.totalLikes([listWithMultipleBlogs[0]])
+    const result = listHelper.totalLikes([listHelper.initialBlogs[0]])
     assert.strictEqual(result, 5)
   })
 
   test('when list has multiple posts, provide the likes total', () => {
-    const result = listHelper.totalLikes(listWithMultipleBlogs)
+    const result = listHelper.totalLikes(listHelper.initialBlogs)
     assert.strictEqual(result, 59)
   })
 
--- part4/blog-list/blog-list-backend/utils/tests/list_helper.js
+++ part4/blog-list/blog-list-backend/utils/tests/list_helper.js
@@ -2,6 +2,49 @@ const dummy = (blogs) => {
   return 1
 }
 
+const initialBlogs = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422bb71b54a676234d17f9',
+    title: 'Reflections on Trusting Trust',
+    author: 'Ken Thompson',
+    url: 'https://www.archive.ece.cmu.edu/~ganger/712.fall02/papers/p761-thompson.pdf',
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: '5a422cc71b54a676234d17fa',
+    title: 'The Mythical Man-Month',
+    author: 'Frederick P. Brooks Jr.',
+    url: 'https://archive.org/details/mythicalmanmonth00broo',
+    likes: 20,
+    __v: 0
+  },
+  {
+    _id: '5a422dd71b54a676234d17fb',
+    title: 'A Note on Distributed Computing',
+    author: 'Leslie Lamport',
+    url: 'https://lamport.azurewebsites.net/pubs/distributed-system.txt',
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: '5a422ee71b54a676234d17fc',
+    title: 'No Silver Bullet — Essence and Accidents of Software Engineering',
+    author: 'Frederick P. Brooks Jr.',
+    url: 'https://www.cs.unc.edu/techreports/86-020.pdf',
+    likes: 15,
+    __v: 0
+  }
+]
+
 /**
  * Only used for linting
  * @param {Array} posts 
@@ -15,4 +58,4 @@ const totalLikes = (posts) => {
   return posts.reduce(reducer, 0)
 }
 
-module.exports = { dummy, totalLikes }
+module.exports = { dummy, initialBlogs, totalLikes }
