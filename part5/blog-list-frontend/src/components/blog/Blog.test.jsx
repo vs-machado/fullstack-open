4ec7397d7cffb0aec15c3ba53cefea3c314741f3
vs-machado <vinicius.s.machado@protonmail.com>
2026-01-23 12:30:46 -0300

    exercise 5.14 done

--- part5/blog-list-frontend/src/components/blog/Blog.test.jsx
+++ part5/blog-list-frontend/src/components/blog/Blog.test.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('blog component displays blogs title and author only initially', () => {
@@ -35,4 +36,44 @@ test('blog component displays blogs title and author only initially', () => {
   const input3 = container.querySelector('#blog-likes')
   expect(input3).toBeDefined()
   expect(input3).toBeNull()
+})
+
+test('blog component displays blogs url and likes after clicking on view button', async () => {
+  const blog = {
+    user: {
+      id: '123'
+    },
+    title: 'Blog title test',
+    author: 'vs-machado',
+    url: 'www.google.com',
+    likes: 1,
+  }
+
+  const user = {
+    id: '123'
+  }
+
+  const mockHandler = vi.fn()
+
+  const { container } = render(<Blog blog={blog} likeBlogPost={mockHandler} removeBlogPost={mockHandler} user={user} />)
+
+  // Before click
+  expect(container.querySelector('#blog-url')).not.toBeInTheDocument()
+  expect(container.querySelector('#blog-likes')).not.toBeInTheDocument()
+
+  // Click action
+  const userE = userEvent.setup()
+  const button = screen.getByText('view')
+  await userE.click(button)
+
+  // After click
+  const inputUrl = container.querySelector('#blog-url')
+  expect(inputUrl).toBeDefined()
+  expect(inputUrl).toBeInTheDocument()
+  expect(inputUrl).toHaveTextContent('www.google.com')
+
+  const inputLikes = container.querySelector('#blog-likes')
+  expect(inputLikes).toBeDefined()
+  expect(inputUrl).toBeInTheDocument()
+  expect(inputLikes).toHaveTextContent('1')
 })
