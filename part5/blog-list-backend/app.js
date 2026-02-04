5e03f2d4ced7dcdc3645f84b67b368142f1c78f7
vs-machado <vinicius.s.machado@protonmail.com>
2026-02-04 14:08:36 -0300

    exercise 5.18 done

--- part5/blog-list-backend/app.js
+++ part5/blog-list-backend/app.js
@@ -6,6 +6,7 @@ const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const testingRouter = require('./controllers/testing')
 
 const app = express()
 
@@ -27,6 +28,10 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part5/e2e-tests/tests/blog_app.spec.js
+++ part5/e2e-tests/tests/blog_app.spec.js
@@ -1,19 +1,68 @@
 import test, { expect, beforeEach, describe } from "@playwright/test";
 
+const BACKEND_URL = 'http://localhost:3002'
 
+// Backend must be started with npm run start:test
 describe('Blog app', () => {
-  beforeEach( async ({ page }) => {
-    await page.goto('http://localhost:5173/')
+  beforeEach( async ({ page, request }) => {
+    // clean db
+    await request.post(`${BACKEND_URL}/api/testing/reset`)
+
+    // user registration    
+    await request.post(`${BACKEND_URL}/api/users`, { 
+      data: {
+        name: 'admin',
+        username: 'admin',
+        password: 'test'
+      }
+     })
+
+    await page.goto('http://localhost:5173/') 
   })
 
-  test('Login form is shown', async ({ page }) => {
-    const titleLocator = page.getByText('log in to application')
-    const userInputLocator = page.getByLabel('username')
-    const pwdInputLocator = page.getByLabel('password')
-    const btnLocator = page.getByRole('button', { name: 'login'})
-    await expect(titleLocator).toBeVisible()
-    await expect(userInputLocator).toBeVisible()
-    await expect(pwdInputLocator).toBeVisible()
-    await expect(btnLocator).toBeVisible()
+  describe('Login', () => {
+    test('Login form is shown', async ({ page }) => {
+      const titleLocator = page.getByText('log in to application')
+      const userInputLocator = page.getByLabel('username')
+      const pwdInputLocator = page.getByLabel('password')
+      const btnLocator = page.getByRole('button', { name: 'login'})
+      await expect(titleLocator).toBeVisible()
+      await expect(userInputLocator).toBeVisible()
+      await expect(pwdInputLocator).toBeVisible()
+      await expect(btnLocator).toBeVisible()
+    })
+
+    test('succeeds with correct credentials', async ({ page }) => {
+      const userInputLocator = page.getByLabel('username')
+      const pwdInputLocator = page.getByLabel('password')
+      const btnLocator = page.getByRole('button', { name: 'login'})
+
+      await userInputLocator.fill('admin')
+      await pwdInputLocator.fill('test')
+      await btnLocator.click()
+
+      await expect(btnLocator).not.toBeVisible()
+
+      const loggedTextLocator = page.getByText('admin logged in')
+      await expect(loggedTextLocator).toBeVisible()
+
+      const createBtn = page.getByRole('button', { name: 'create new blog' })
+      await expect(createBtn).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      const userInputLocator = page.getByLabel('username')
+      const pwdInputLocator = page.getByLabel('password')
+      const btnLocator = page.getByRole('button', { name: 'login'})
+
+      await userInputLocator.fill('anotheruser')
+      await pwdInputLocator.fill('test123')
+      await btnLocator.click()
+
+      await expect(btnLocator).toBeVisible()
+      
+      const loggedTextLocator = page.getByText('admin logged in')
+      await expect(loggedTextLocator).not.toBeVisible()
+    })
   })
 })
