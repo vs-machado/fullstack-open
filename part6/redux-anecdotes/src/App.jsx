3dc56e764f0c0b490527ede2823cf4f9e1c265a9
vs-machado <vinicius.s.machado@protonmail.com>
2026-03-03 15:40:52 -0300

    exercises 6.3 and 6.4 done

---
+++ part6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,13 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import Anecdotes from './components/Anecdotes'
+
+const App = () => {
+  return (
+    <div>
+      <Anecdotes />
+      <AnecdoteForm />
+    </div>
+  )
+}
+
+export default App
