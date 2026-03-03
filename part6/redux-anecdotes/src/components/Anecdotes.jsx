9e126e8a859736a6fe2305716b61cf2c830d3723
vs-machado <vinicius.s.machado@protonmail.com>
2026-03-03 15:52:33 -0300

    exercise 6.5 done

--- part6/redux-anecdotes/src/components/Anecdotes.jsx
+++ part6/redux-anecdotes/src/components/Anecdotes.jsx
@@ -16,11 +16,12 @@ const Anecdote = ({ anecdote, handleClick }) => {
 const Anecdotes = () => {
   const dispatch = useDispatch()
   const anecdotes = useSelector(state => state)
+  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote => (
+      {sortedAnecdotes.map(anecdote => (
         <div>
           <Anecdote 
             key={anecdote.id}
